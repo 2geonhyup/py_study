5fcb87787bd83070c0d95e93a6be5bee368763ff
2geonhyup <alex9750@korea.ac.kr>
2021-01-28 13:22:34 +0900

    geo

--- 1.py
+++ 1.py
@@ -1,14 +1,30 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
 from typing import *
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+    def function(self, s: str) -> bool:
+        strs = []
+        for char in s:
+            if char.isalnum():  # 숫자와 문자만 남깁니다
+                strs.append(char.lower())
+        # print(strs)
+        strB = []
+        strB = list(reversed(strs))  # 거꾸로 뒤집힌 리스트를 반환합니다
+        # print(strB)
+        if strs == strB:
+            return True
+        else:
+            return False
 
 
 sol = Solution()
-print(sol.twoSum([2, 7, 11, 15], 9))
+print(sol.function("A man, a plan, a canal: Panama"))
 # practice
